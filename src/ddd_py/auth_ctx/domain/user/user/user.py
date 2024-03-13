cb48aa0e67fe6f578173e15a1685ba23656cd957
wwwshwww <www.shinderu.www@gmail.com>
2024-03-14 01:41:43 +0900

    add prop in user domain of auth_ctx

--- src/ddd_py/auth_ctx/domain/user/user/user.py
+++ src/ddd_py/auth_ctx/domain/user/user/user.py
@@ -6,3 +6,12 @@ class User():
     def __init__(self, id: Id, google_profile: GoogleProfile):
         self._id = id
         self._google_profile = google_profile
+
+    @property
+    def id(self) -> Id:
+        return self._id
+    
+    @property
+    def google_profile(self) -> GoogleProfile:
+        return self._google_profile
+    

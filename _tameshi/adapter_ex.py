ab1ce0fe8449bc8cc7d1f05018b98953cf064f0b
wwwshwww <www.shinderu.www@gmail.com>
2024-03-17 00:01:13 +0900

    update FastAPI sandbox

--- _tameshi/adapter_ex.py
+++ _tameshi/adapter_ex.py
@@ -1,7 +1,11 @@
 import asyncio
+from collections.abc import Generator
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 router = APIRouter()
@@ -23,11 +27,11 @@ class Usecase2:
         return f"{self.repo}2"
 
 
-def gen_dependencies():
+def gen_dependencies() -> Generator[tuple[Usecase1, Usecase2], None, None]:
     raise NotImplementedError("need to inject dependency")
 
 
-def mock_dependencies():
+def mock_dependencies() -> Generator[tuple[Usecase1, Usecase2], None, None]:
     try:
         print("mock だよ")
         yield Usecase1("mock"), Usecase2("mock")
@@ -35,13 +39,34 @@ def mock_dependencies():
         print("clean up mock")
 
 
-@router.get("/")
+class MyResponse(BaseModel):
+    hello: str
+    world: str
+
+
+@router.get(
+    "/",
+    status_code=302,
+    responses={
+        302: {
+            "description": "ほげ",
+            "model": MyResponse,
+            "headers": {"Location": {"schema": {"type": "string"}}},
+        }
+    },
+)
 async def read_root(
     deps: Annotated[tuple[Usecase1, Usecase2], Depends(gen_dependencies)],
+    p: str | None = None,
 ):
+    print(p)
     uc1, uc2 = deps
     out1, out2 = await asyncio.gather(uc1.get1(), uc2.get2())
-    return {"Hello": out1, "World": out2}
+    headers = {"Location": "/docs"}
+    content = MyResponse(hello=out1, world=out2)
+    return JSONResponse(
+        status_code=302, content=jsonable_encoder(content), headers=headers
+    )
 
 
 app.include_router(router)

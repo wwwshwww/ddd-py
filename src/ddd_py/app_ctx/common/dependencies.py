0ca6e8f3ed84754aeb54496d70d5b3a473af1e0d
wwwshwww <www.shinderu.www@gmail.com>
2024-05-07 01:03:23 +0900

    Restructure "post generation" of domain object

--- src/ddd_py/app_ctx/common/dependencies.py
+++ src/ddd_py/app_ctx/common/dependencies.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 
 from ddd_py.app_ctx.usecase import (
     find_post,
-    find_post_generate_request,
     find_reaction,
     find_reaction_preset,
     find_user,
@@ -13,7 +12,6 @@ from ddd_py.app_ctx.usecase import (
 @dataclass
 class Dependencies:
     usecase_find_post: find_post.Usecase
-    usecase_find_post_generate_request: find_post_generate_request.Usecase
     usecase_find_reaction: find_reaction.Usecase
     usecase_find_reaction_preset: find_reaction_preset.Usecase
     usecase_find_user: find_user.Usecase
